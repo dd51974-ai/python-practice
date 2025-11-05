8fc91c52488a31b7804f2ebb18c3336996851925
Ai Tsujikawa <tuji.kamome@gmail.com>
2025-11-05 20:55:07 +0900

    Wrote the code to IF, input

--- main.py
+++ main.py
@@ -2,11 +2,20 @@
 
 # Press ⌃R to execute it or replace it with your code.
 # Press Double ⇧ to search everywhere for classes, files, tool windows, actions, and settings.
+number = int(input("Press Number something."))
 
+if number > 5:
+    print("X is more than 5.")
+else:
+    print("X is less than 5.")
 
 def print_hi(name):
     # Use a breakpoint in the code line below to debug your script.
+    name = input("What's your name?")
     print(f'Hi, {name}')  # Press ⌘F8 to toggle the breakpoint.
+    car = input("What do you want a wheel?")
+    print(f'I want {car}')
+
 
 
 # Press the green button in the gutter to run the script.
